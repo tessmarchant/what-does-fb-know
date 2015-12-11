2c000a0da6976818305dc2732ec7435190a1ea84
Tess Marchant <tessmmarchant@gmail.com>
2015-12-11 13:33:29 -0500

    Built basic survey page
    No submission yet

---
+++ app/app.js
@@ -0,0 +1,24 @@
+(function(){
+
+var app = angular.module('what-does-fb-know', [
+  'ngRoute',
+  'what-does-fb-know.controllers'
+]);
+
+var cont = angular.module('what-does-fb-know.controllers', [ ]);
+
+app.config(function ($routeProvider) {
+
+  // configure view routing
+  $routeProvider
+    .when('/', {
+      controller: 'surveyController',
+      templateUrl: 'app/partials/survey.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+});
+
+})();
---
+++ app/controllers/surveyController.js
@@ -0,0 +1,56 @@
+(function(){
+
+var cont = angular.module('what-does-fb-know.controllers');
+
+cont.controller('surveyController', function ($scope, $filter, $http, $location) {
+
+  $(document).ready(function (){
+    $scope.responses = {};
+    $scope.responses.age = "";
+    $scope.responses.sex = "";
+    $scope.responses.empinfo = "4";
+    $scope.responses.finbeh = "4";
+    $scope.responses.chardon = "4";
+    $scope.responses.expinfo = "4";
+    $scope.responses.resprof = "4";
+    $scope.responses.travbeh = "4";
+    $scope.responses.purchbeh = "4";
+    $scope.responses.medcon = "4";
+  });
+
+  $scope.submit = function () {
+    console.log($scope.responses);
+  };
+
+  $scope.privacyMeterLabel = function (privacyLevel) {
+    console.log(privacyLevel);
+    var text = '';
+    if (privacyLevel == 1) {
+      text = 'Very Private';
+    }
+    else if (privacyLevel == 2) {
+      text = 'Fairly Private';
+    }
+    else if (privacyLevel == 3) {
+      text = 'Somewhat Private';
+    }
+    else if (privacyLevel == 4) {
+      text = 'Neither Private nor Public';
+    }
+    else if (privacyLevel == 5) {
+      text = 'Somewhat Public';
+    }
+    else if (privacyLevel == 6) {
+      text = 'Fairly Public';
+    }
+    else if (privacyLevel == 7) {
+      text = 'Very Public';
+    }
+    else {
+      text = 'error';
+    }
+    return text;
+  };
+});
+
+})();
