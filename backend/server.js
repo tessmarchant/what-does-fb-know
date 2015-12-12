c453366006d0cdbe14b6533436f0a3e359dfe093
Tess Marchant <tessmmarchant@gmail.com>
2015-12-12 14:39:22 -0500

    Survey submission to database works

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,59 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 //app.use(multer()); // for parsing multipart/form-data
 
+/* MongoDB Setup */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/what-does-fb-know');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('mongoose: Connected to database \'what-does-fb-know\'');
+});
+
+
+var surveySchema = mongoose.Schema({
+	age: String,
+	sex: String,
+	rankings: {
+		empinfo: Number,
+		finbeh: Number,
+    	chardon: Number,
+    	expinfo: Number,
+    	resprof: Number,
+    	travbeh: Number,
+    	purchbeh: Number,
+    	medcon: Number
+	}
+});
+
+var Survey = mongoose.model('Survey', surveySchema);
+
+/*var statsSchema = mongoose.Schema({
+	allStats =
+});*/
+
 app.post('/submitsurvey', function (req, res) {
 	console.log('POST request to /submitsurvey');
-	console.log(req.body);
-	// Store and process the survey data here
+	var data = req.body;
+
+	var newSurvey = {};
+	newSurvey.age = data.age;
+	newSurvey.sex = data.sex;
+	newSurvey.rankings = {};
+	newSurvey.rankings.empinfo = data.empinfo;
+	newSurvey.rankings.finbeh  = data.finbeh;
+	newSurvey.rankings.chardon = data.chardon;
+	newSurvey.rankings.expinfo = data.expinfo;
+	newSurvey.rankings.resprof = data.resprof;
+	newSurvey.rankings.travbeh = data.travbeh;
+	newSurvey.rankings.purchbeh = data.purchbeh;
+	newSurvey.rankings.medcon 	= data.medcon;
+
+	var object = new Survey(newSurvey);
+	object.save(function (err) {
+		if (err) return console.error(err);
+	});
+
 	res.end();
 });
 
